60e7756a83e01be95eee00c9f34849ba42978adc
William Ward <wward246@gmail.com>
2020-07-25 22:33:59 -0700

    update

--- src1/views/home/index.php
+++ src1/views/home/index.php
@@ -74,19 +74,20 @@
 					 He also went out of his way to shuttle me from my home to work and back again, which was really helpful and made it easy on me. 
 					 A few months after the repair, I had a second incident where the car rear got scuffed, I didn't hesitate to give Jared a call. 
 					He made it look better than new!</q></h2>
-				  
+				  <p></p>	
 				  <img class="testimonialImage" src="assets/images/Testimonials/stephen_alter.jpg"  alt="wrapkit" >
 							  <p class="author">- Stephen Alter</p>
 				</div>
 				<div class="swiper-slide" >
 				  <h2 class="text"><q>Ace Parking has been using Urgent Care Bumper Repair for 7 or more years for our own customers. Its inevitable to have our self-parking customers get a scuff or scrape or two and we always refer them to Jared. Urgent Care Bumper Repair comes out to our lots and fixes the car on the spot, so it has been a win-win situation for all involved. Well, it wasn't too long before I had my own need for repair on my BMW M5. As the Owner of Ace, I always heard wonderful feedback about the work Jared did for our customers....</q></h2>
+				  <p></p>	
 				  <img class="testimonialImage"  src="assets/images/Testimonials/keith_jones.jpg" alt="wrapkit" >	
 					<p class="author">- Keith Jones</p>
 				</div>
 				<div class="swiper-slide" >
 				  <h2 class="text"><q>Jared fixed the damage on my jeep a couple of different times. I have had a couple accidents, but been very luck to know someone like Jared to call on when one happens. 
 					 He comes out to my house in Encinitas, takes just a couple of hours and my jeep looks as good as new when he is done. Its great to have his help, all I have to do is call him and I don't worry about it after that. He is so helpful, handling calls between insurance companies and dealers for parts, so that I don't have to... </q></h2>
-				  
+				  <p></p>	
 				  <img class="testimonialImage"  src="assets/images/Testimonials/vicky_guillen.jpg" alt="wrapkit" >
 				  <p class="author">- Vicky Guillen</p>
 				</div>
@@ -94,6 +95,7 @@
 				  <h2 class="text"><q>The quality of the job Jared did on my Audi is remarkable. He really paid attention to details and treated it like it was his own car. Its like the damage never happened.
 					 There are a lot of places out there to choose from, but this is the only guy I call because I have seen his work and he really knows what he is doing.
 					 Its so convenient too, He came to my home and in a few of hours it was beautifully done. I was so impressed that I referred him to repair my boss' Porsche and he did an awesome job on that too.</q></h2>
+					 <p></p>	
 				  <img class="testimonialImage"  src="assets/images/Testimonials/steve_shanahan.jpg"  alt="wrapkit" >
 				  <p class="author">- Steve Shanahan</p>
 				</div>
