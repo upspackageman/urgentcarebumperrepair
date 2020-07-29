daa2464c0990014b45a29210ad9f4989dcb594e2
William Ward <wward246@gmail.com>
2020-07-28 17:39:41 -0700

    Update

--- src1/assets/js/homeSwiper.js
+++ src1/assets/js/homeSwiper.js
@@ -3,7 +3,7 @@ var lexus1 = new Swiper('.lexus1', {
     slidesPerView: 'auto',
     loop: true,
     coverflowEffect: {
-      rotate: 50,
+      rotate: 100,
       stretch: 0,
       depth: 100,
       modifier: 1,
