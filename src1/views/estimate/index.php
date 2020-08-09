b9751af3e0a27f36c2a2d51c0defba7cd163b37f
William Ward <wward246@gmail.com>
2020-08-08 18:01:26 -0700

    update

--- src1/views/estimate/index.php
+++ src1/views/estimate/index.php
@@ -11,7 +11,7 @@
         <h2 class="text-white font-weight-normal op-8"></h2>
         <p></p>
         <div style="background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))">
-          <img src="assets/images/Import images/2020-04-29/238a.png" style="border-style: inset; border-radius: 3px; border-width: 20px; border-color:rgba(98, 83, 70, 0.32);" class="captionImages" alt="...">
+          <img src="assets/images/Import images/2020-04-29/estimate/238a.png" style="border-style: inset; border-radius: 3px; border-width: 20px; border-color:rgba(98, 83, 70, 0.32);" class="captionImages" alt="...">
         </div>
         <p></p>
         <h2 class="text-white font-weight-normal op-8">It’s very easy to get your FREE ONLINE OR PHONE ESTIMATE.</h2>
@@ -19,10 +19,10 @@
         <h5 class="text-white font-weight-normal op-8">Just snap a couple of photos of your damage with your digital camera or smart phone.</h5> 
         <p></p>
         <h5 class="text-white font-weight-normal op-8 ">Close up image of the damage.</h5>
-        <img src="assets/images/Import images/2020-04-29/207a.jpg" style="border-style: inset; border-radius: 3px; border-width: 20px; border-color:rgba(98, 83, 70, 0.32);" class="captionImages" alt="...">
+        <img src="assets/images/Import images/2020-04-29/estimate/207a.jpg" style="border-style: inset; border-radius: 3px; border-width: 20px; border-color:rgba(98, 83, 70, 0.32);" class="captionImages" alt="...">
         <p></p>
         <h5 class="text-white font-weight-normal op-8">Full shot of the vehicle and damage.</h5>
-        <img src="assets/images/Import images/2020-04-29/207.jpg" style="border-style: inset; border-radius: 3px; border-width: 20px; border-color:rgba(98, 83, 70, 0.32);"  class="captionImages" alt="...">
+        <img src="assets/images/Import images/2020-04-29/estimate/207.jpg" style="border-style: inset; border-radius: 3px; border-width: 20px; border-color:rgba(98, 83, 70, 0.32);"  class="captionImages" alt="...">
         <p></p>
         <h5 class="text-white font-weight-normal op-8">Provide me your make, model and year.</h5>
         <h5 class="text-white font-weight-normal op-8">I’ll always get back to you within the business day, usually within a few hours.</h5>
